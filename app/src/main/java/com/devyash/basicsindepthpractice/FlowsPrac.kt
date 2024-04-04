1259cfc5cadbad4b0fa1aaca6f7046849b28291c
Yashveer singh <yashveersingh30401@gmail.com>
2024-04-04 18:42:13 +0530

    Filter in flows

--- app/src/main/java/com/devyash/basicsindepthpractice/FlowsPrac.kt
+++ app/src/main/java/com/devyash/basicsindepthpractice/FlowsPrac.kt
@@ -10,30 +10,33 @@ import androidx.lifecycle.lifecycleScope
 import com.devyash.basicsindepthpractice.databinding.ActivityFlowsPracBinding
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
 class FlowsPrac : AppCompatActivity() {
 
-    private var _binding:ActivityFlowsPracBinding?=null
+    private var _binding: ActivityFlowsPracBinding? = null
     private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityFlowsPracBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-            val collectPersons = emitPersons()
-           lifecycleScope.launch {
-               collectPersons.collect{person->
-                   for(i in 0..person.size-1){
-                       Log.d("FLOWSPRAC",person.get(i).toString())
-                   }
-               }
-           }
+        val collectPersons = emitPersons()
+        lifecycleScope.launch {
+            collectPersons.filter {
+                it.age > 30
+            }.collect { person ->
+
+                Log.d("FLOWSPRAC", person.toString())
+
+            }
+        }
 
     }
 
-    fun emitPersons(): Flow<List<Person>> {
+    fun emitPersons(): Flow<Person> {
         return flow {
             val persons = listOf(
                 Person("John Doe", 30, "New York", "Software Engineer", 100000),
@@ -47,11 +50,19 @@ class FlowsPrac : AppCompatActivity() {
                 Person("Benjamin Anderson", 32, "Dallas", "Writer", 95000),
                 Person("Isabella Thomas", 36, "San Jose", "Entrepreneur", 200000)
             )
-            emit(persons)
+            for (i in 0..persons.size - 1) {
+                emit(persons.get(i))
+            }
         }
     }
 
-    data class Person(val name:String, val age:Int, val address:String, val job:String, val salary:Int)
+    data class Person(
+        val name: String,
+        val age: Int,
+        val address: String,
+        val job: String,
+        val salary: Int
+    )
 
     override fun onDestroy() {
         super.onDestroy()
